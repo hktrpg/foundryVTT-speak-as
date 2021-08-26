ebfc4c23c2edcbe4ea19288396dc1df0f0f4a673
zeteticl <cougar.l@hotmail.com>
2021-08-26 18:32:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var isDefaultName = false;
-
+var innerHTML;
 Hooks.once("init", () => {
+
+    console.log('AAAAAAAAAAAAAAAAAs')
     /*
     game.settings.register("default_name", "isDefaultName", {
         name: 'is Default Name',
@@ -31,12 +33,61 @@ i)可選自己的身份或自己擁有的TOKEN
 3)沒有的話，則把發言名稱改成那個名字
 
 
+  <label class="chat-control-icon"><i class="fas fa-dice-d20"></i></label>
+        <select class="roll-type-select" name="rollMode">
+            <optgroup label="Default Roll Mode">
+            <option value="roll">Public Roll</option>
+            <option value="gmroll">Private GM Roll</option>
+            <option value="blindroll" selected="">Blind GM Roll</option>
+            <option value="selfroll">Self Roll</option>
+            </optgroup>
+        </select>
+
+        <div class="control-buttons">
+            <a class="button export-log" title="Export Chat Log"><i class="fas fa-save"></i></a>
+            <a class="delete button chat-flush" title="Clear Chat Log"><i class="fas fa-trash"></i></a>
+        </div>
+
 */
 
+
+
+Hooks.on("renderSidebarTab", (dialog, $element, ABC) => {
+    var HTML;
+    let addText = `<select name="cars" id="cars"  class="roll-type-select">
+    <optgroup label="Default Speak As....">
+    <option value="volvo">Volvo</option>
+    <option value="saab">Saab</option>
+    <option value="mercedes">Mercedes</option>
+    <option value="audi">Audi</option>
+  </select>`;
+    /**
+     * 自己的登入名字
+     * 自己擁有的角色
+    */
+    ($element.find(`div#chat-controls.flexrow`)) ? HTML = $element.find(`div#chat-controls.flexrow`) : null;
+    if (HTML[0]) {
+        console.log(HTML[0].innerHTML)
+        //innerHTML = HTML[0].innerHTML;
+        HTML[0].innerHTML = addText + HTML[0].innerHTML;
+
+    }
+
+    //  innerHTML[0].innerHTML = ""
+
+    // outerHTML
+
+    //  innerHTML.prevObject[0].outerHTML=''
+    /// ABC.speaker.token = "XXX"
+    //2)如場上有同樣的TOKEN，使用那個TOKEN發言
+    //ABC.speaker.actor = '';
+    //ABC.speaker.alias = 'XXX';
+});
+
 Hooks.on("chatMessage", (dialog, $element, ABC) => {
-    console.log('dialog', dialog)
-    console.log('$element', $element)
-    console.log('ABC', ABC)
+    //   console.log('dialog', dialog)
+    // console.log('$element', $element)
+    // console.log('ABC', ABC)
 
     ABC.speaker.token = "XXX"
     //2)如場上有同樣的TOKEN，使用那個TOKEN發言
